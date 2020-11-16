a81aa9e0092f45bbf219c5423a5d16bfdb230cfc
young <youngdev.21c@gmail.com>
2020-11-16 21:46:53 +0900

    BOJ|10872|팩토리얼

--- BOJ/BOJ_10872_팩토리얼.py
+++ BOJ/BOJ_10872_팩토리얼.py
@@ -1,8 +1,18 @@
 
 
-N = int(input())
+# N = int(input())
+#
+# res = 1
+# for n in range(N, 0, -1):
+#     res = res*n
+# print(res)
 
-res = 1
-for n in range(N, 0, -1):
-    res = res*n
-print(res)
+def factorial(N):
+    if N == 1:
+        return 1
+    elif N == 0 :
+        return 1
+    return N *factorial(N-1)
+
+N = int(input())
+print(factorial(N))
