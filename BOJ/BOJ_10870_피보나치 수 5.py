5c7f61541cba19aab5e93d9cfa8360ea806b2e66
young <youngdev.21c@gmail.com>
2020-11-18 22:37:07 +0900

    BOJ | 10870

--- BOJ/BOJ_10870_피보나치 수 5.py
+++ BOJ/BOJ_10870_피보나치 수 5.py
@@ -1,12 +1,9 @@
 
 
-#이왜틀
-# def Fibbo(n):
-#     if n == 0:
-#         return 0
-#     elif n == 1:
-#         return 1
-#     return n + Fibbo(n-1)
-#
-# n = int(input())
-# print(Fibbo(n))
+def Fibbo(n):
+    if n<=1:
+        return n
+    return Fibbo(n-1) + Fibbo(n-2)
+
+n = int(input())
+print(Fibbo(n))
