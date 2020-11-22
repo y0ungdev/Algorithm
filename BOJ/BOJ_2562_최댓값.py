74900d4339afc507956d7c71653f94f95f76d638
young <youngdev.21c@gmail.com>
2020-11-22 23:03:49 +0900

    1122

--- BOJ/BOJ_2562_최댓값.py
+++ BOJ/BOJ_2562_최댓값.py
@@ -1,6 +1,12 @@
 
-for _ in range(9):
-    nums = int(input())
+# for _ in range(9):
+# #     nums = int(input())
+# #
+# #     print(max(nums))
+# #     print(max(nums[i]))
+nums_list = []
+for i in range(9):
+    nums_list.append(int(input()))
 
-    print(max(nums))
-    print(max(nums[i]))
+print(max(nums_list))
+print(nums_list.index(max(nums_list))+1)
